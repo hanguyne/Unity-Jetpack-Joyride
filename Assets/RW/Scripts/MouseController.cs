28451732c3fd3cb91526c50746f2439559d9d9b2
Hanguyen <ha.thinguyen2205@gmail.com>
2025-11-13 00:18:41 +0700

    JetpackJoyridePart2

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -3,26 +3,59 @@ using UnityEngine;
 public class MouseController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+    private Animator mouseAnimator;
+    public ParticleSystem jetpack;
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        var jetpackEmission = jetpack.emission;
+        jetpackEmission.enabled = !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackEmission.rateOverTime = 300.0f;
+        }
+        else
+        {
+
+            jetpackEmission.rateOverTime = 75.0f;
+        }
+    }
+
+
     private Rigidbody2D playerRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-
+        mouseAnimator = GetComponent<Animator>();
     }
+    void UpdateGroundedStatus()
+    {
+        //1
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        //2
+        mouseAnimator.SetBool("isGround", isGrounded);
+    }
+
     void FixedUpdate() 
 {
     bool jetpackActive = Input.GetButton("Fire1");
-    if (jetpackActive)
-    {
-        playerRigidbody.AddForce(new Vector2(0, jetpackForce));
-    }
+        if (jetpackActive)
+        {
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+        }
+    Vector2 newVelocity = playerRigidbody.linearVelocity;
+    newVelocity.x = forwardMovementSpeed;
+        playerRigidbody.linearVelocity = newVelocity;
+        AdjustJetpack(jetpackActive);
+    UpdateGroundedStatus();
+
+
+
 }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
